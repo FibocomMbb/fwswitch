80c5fd16da052a9aac55c120a1fb29818b6096d8
FibocomMbb <mbb.softone@fibocom.com>
2019-02-19 10:15:52 +0800

    add user-prepare-fwswitch.sh

---
+++ user-prepare-fwswitch.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+REMOTEKEYPATH="https://github.com/FibocomMbb/fwswitch/raw/master/apt/key/deb.gpg.key "
+KEY="./.tmp_key"
+REPOSITORY_PATH="deb https://github.com/FibocomMbb/fwswitch/raw/master/apt/ fwswitch main"
+SOURCE_PATH="/etc/apt/sources.list"
+if [ $(whoami) != "root" ]; then
+	echo "Please run as root"
+	exit 1
+fi
+
+wget -O $KEY $REMOTEKEYPATH  
+rc=$?
+if [ $rc -ne 0 ]; then
+	echo "Download key failed, please check the Network, if Network is ok, please copy the output and connect Fibocom"
+	exit 1
+else
+	echo "Update the fwswitch gpg key"
+fi
+cat $KEY | apt-key add -
+rm $KEY
+
+cat $SOURCE_PATH | grep "${REPOSITORY_PATH}"
+rc=$?
+if [ $? -eq 0 ]; then
+	echo "The repository url exist, NOT add it again"
+else
+	echo "The repository url NOT exist, add it in "$SOURCE_PATH
+	echo $REPOSITORY_PATH | tee -a $SOURCE_PATH
+fi
+
+apt-get update
+echo "Now you can run \"sudo apt-get install fwswitchtool\" get or update the fwswitch"
